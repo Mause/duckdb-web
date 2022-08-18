37b8773b557e927e4e2af6d43a1326f1bb611b81
Elliana May <me@mause.me>
2022-08-18 13:04:56 +0800

    chore: add update-docs.yml workflow

--- scripts/generate_all_docs.sh
+++ scripts/generate_all_docs.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env sh
 
-python3 ./scripts/generate_config_docs.py
-python3 ./scripts/generate_descriptions.py
-python3 ./scripts/generate_docs.py
+$DUCKDB="$1";
+
+python3 ./scripts/generate_config_docs.py $DUCKDB
+python3 ./scripts/generate_descriptions.py $DUCKDB
+python3 ./scripts/generate_docs.py $DUCKDB
 python3 ./scripts/generate_python_docs.py
-node ./scripts/generate_nodejs_docs.js
+node ./scripts/generate_nodejs_docs.js $DUCKDB
 
 # generate search index last, once all the docs are generated
 python3 ./scripts/generate_search.py
