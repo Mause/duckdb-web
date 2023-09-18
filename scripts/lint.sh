b557b53a973929e5fe21da18c225b39a20c48db8
Elliana May <me@mause.me>
2023-09-18 09:00:58 +0800

    update lint

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -15,6 +15,7 @@ done
 npx markdownlint-cli docs/ dev/ _posts/ --config .markdownlint.jsonc --ignore docs/archive $fix || echo 'mdlit failed'
 
 black scripts --skip-string-normalization $check || echo 'black failed'
+find . -name '*.md' | grep -v node_modules | grep -v vendor | grep -v archive | xargs blacken-docs-jb -S || echo 'blacken-docs failed'
 
 vale sync
 vale docs/ dev/ _posts/ --glob "!docs/archive/*"
